088daa47edfd9d56fd8f00ff10069104b9ff5bda
Naufal Ihsan <naufal.ihsan21@gmail.com>
2019-02-12 20:00:32 +0700

    Updates

---
+++ precache-manifest.d9bd3b1d97361e96b66748b9d22e19fb.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5400798c725649e6844c960bcf3ea7e",
+    "url": "/payment-simulator/static/media/Progress3.c5400798.svg"
+  },
+  {
+    "revision": "b0419c167ee738b323ac",
+    "url": "/payment-simulator/static/css/main.cb32c94e.chunk.css"
+  },
+  {
+    "revision": "b826247c8af3b26abd21",
+    "url": "/payment-simulator/static/js/runtime~main.b826247c.js"
+  },
+  {
+    "revision": "ea7210beb7cf5c32c220",
+    "url": "/payment-simulator/static/js/2.ea7210be.chunk.js"
+  },
+  {
+    "revision": "e7f90c20c4e81eb44fd043c9befea585",
+    "url": "/payment-simulator/static/media/Tooltips.e7f90c20.svg"
+  },
+  {
+    "revision": "0921149e623692200ad63b0e29f22c00",
+    "url": "/payment-simulator/static/media/Table.0921149e.svg"
+  },
+  {
+    "revision": "5ea27c5659269d38c7b033c29db629d4",
+    "url": "/payment-simulator/static/media/UI.5ea27c56.svg"
+  },
+  {
+    "revision": "13c7bc8be332b943ce755f12690495ba",
+    "url": "/payment-simulator/static/media/Leaves.13c7bc8b.svg"
+  },
+  {
+    "revision": "7197ad377c0faa0d81fab5656e2ea04d",
+    "url": "/payment-simulator/static/media/Leaves-1.7197ad37.svg"
+  },
+  {
+    "revision": "29e7c5df06958c37ae36022b9c438123",
+    "url": "/payment-simulator/static/media/Girl1.29e7c5df.svg"
+  },
+  {
+    "revision": "7246a4e9a0698b5ae003296d053cf0fb",
+    "url": "/payment-simulator/static/media/Progress.7246a4e9.svg"
+  },
+  {
+    "revision": "5451ff949ace4fdb783dee3661608a34",
+    "url": "/payment-simulator/static/media/Progress1.5451ff94.svg"
+  },
+  {
+    "revision": "13c47a605722f7b6127a98a3ec24858b",
+    "url": "/payment-simulator/static/media/Progress2.13c47a60.svg"
+  },
+  {
+    "revision": "b0419c167ee738b323ac",
+    "url": "/payment-simulator/static/js/main.b0419c16.chunk.js"
+  },
+  {
+    "revision": "2d0f6d9b9dab56155c63fdee3ad475ef",
+    "url": "/payment-simulator/static/media/Progress4.2d0f6d9b.svg"
+  },
+  {
+    "revision": "e3f026fe8c3c097a8ee8002506e2fa8e",
+    "url": "/payment-simulator/static/media/Seller.e3f026fe.svg"
+  },
+  {
+    "revision": "1e83c633a4d50bb3dfd281e9bc69a8bc",
+    "url": "/payment-simulator/static/media/Seller1.1e83c633.svg"
+  },
+  {
+    "revision": "38d76093073da6a750d2f216165d8935",
+    "url": "/payment-simulator/static/media/Seller2.38d76093.svg"
+  },
+  {
+    "revision": "1df3affe036a14ae4910288a2e21d553",
+    "url": "/payment-simulator/static/media/Seller3.1df3affe.svg"
+  },
+  {
+    "revision": "e5c956dbdfe8831937e4d71bf1de38e5",
+    "url": "/payment-simulator/static/media/Seller4.e5c956db.svg"
+  },
+  {
+    "revision": "86d11dd8bb8809f6561c5d1dfc6844c8",
+    "url": "/payment-simulator/static/media/Girl.86d11dd8.svg"
+  },
+  {
+    "revision": "dfe4be00efed63a9b4df05ef540d4a37",
+    "url": "/payment-simulator/static/media/Check.dfe4be00.svg"
+  },
+  {
+    "revision": "39de7c0151821045a9fbfb4b0a1c34ff",
+    "url": "/payment-simulator/static/media/Cross.39de7c01.svg"
+  },
+  {
+    "revision": "0a08efd4fa0dfb3c45f004803ca8976e",
+    "url": "/payment-simulator/static/media/Receipt.0a08efd4.svg"
+  },
+  {
+    "revision": "cc069af6a9570d0612c98b20c25bfb72",
+    "url": "/payment-simulator/static/media/BigReceipt.cc069af6.svg"
+  },
+  {
+    "revision": "73abf6c1f6c8492d4131f4db14e793cc",
+    "url": "/payment-simulator/index.html"
+  }
+];
